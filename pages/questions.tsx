9e23018bbebe788d27581dbee273ed7e0cd88f5f
Samuel Yusuf <zulkarnaensamuel1@gmail.com>
2022-06-28 15:52:58 +0700

    feat: Questions page with data fetching and error handling

--- pages/questions.tsx
+++ pages/questions.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Container from "../components/Container/Container";
-import Header from "../components/Header/Header";
+import { useQuizDifficulty } from "../hooks/useQuizDifficulty";
+import { fetchQuestions, QuestionState } from "./api/api";
 
 export default function Questions() {
+  const [state, actions] = useQuizDifficulty();
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startQuiz = async () => {
+    try {
+      setLoading(true);
+      setGameOver(false);
+      const quizQuestions = await fetchQuestions(state.quizDifficulty);
+      setQuestions(quizQuestions);
+      setLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    startQuiz();
+  }, []);
+
   return (
     <Container>
-      <h1>Questons</h1>
+      <h1>Here are the questions</h1>
+      {loading ? <p>Loading...</p> : null}
     </Container>
   );
 }
