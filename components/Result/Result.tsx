43d930d02064ab399dd98cf2e82ce71b95f82f5c
Samuel Yusuf <zulkarnaensamuel1@gmail.com>
2022-06-29 17:06:18 +0700

    feat(questions): result styling + actions
    actions: added return to home button

--- components/Result/Result.tsx
+++ components/Result/Result.tsx
@@ -1,4 +1,7 @@
+import styles from "./Result.module.scss";
+
 import { AnswerType } from "../QuestionCard/QuestionCard";
+import Router from "next/router";
 
 export type ResultProps = {
   answers: AnswerType[];
@@ -11,18 +14,24 @@ export const Result: React.FC<ResultProps> = ({
   score,
   totalScore,
 }) => {
+  const goToHome = () => {
+    Router.push("/");
+  };
+
   return (
-    <div className="result__container">
+    <div className={styles.result__container}>
       <table>
         <tr>
           <th>Question</th>
           <th>Your Answer</th>
           <th>Correct Answer</th>
         </tr>
-        {answers.map((answer) => (
+        {answers.map((answer, i) => (
           <tr
-            className={`${answer.isCorrect ? "correct" : "incorrect"}`}
-            key={answer}
+            className={`${
+              answer.isCorrect ? styles.correct : styles.incorrect
+            }`}
+            key={i}
           >
             <td>
               <span dangerouslySetInnerHTML={{ __html: answer.question }} />
@@ -38,8 +47,13 @@ export const Result: React.FC<ResultProps> = ({
           </tr>
         ))}
       </table>
-      <div className="result__score">
-        {score} / {totalScore}
+      <div className={styles.result__action_bar}>
+        <div className={styles.result__score}>
+          Score: {score} / {totalScore}
+        </div>
+        <button className={styles.result__finish_btn} onClick={goToHome}>
+          Finish
+        </button>
       </div>
     </div>
   );
